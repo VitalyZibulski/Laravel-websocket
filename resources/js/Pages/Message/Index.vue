b5623e592244ae2dffec1ca3aa6504580c4d2c21
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2023-06-05 18:46:46 +0300

    add send message websocket

--- resources/js/Pages/Message/Index.vue
+++ resources/js/Pages/Message/Index.vue
@@ -31,6 +31,14 @@ export default {
             body: ''
         }
     },
+
+    created() {
+        window.Echo.channel('store_message')
+            .listen('.store_message', res => {
+                this.messages.unshift(res.message);
+            });
+    },
+
     methods: {
         store() {
             axios.post('/messages', {
